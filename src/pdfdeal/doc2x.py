87b3f3d997bc66f6e0aae6700837eb36fcd6c8ce
Menghuan <Menghuan2003@outlook.com>
2024-05-31 22:38:51 +0800

    Bug fix

--- src/pdfdeal/doc2x.py
+++ src/pdfdeal/doc2x.py
@@ -2,6 +2,7 @@ import requests
 import json
 import os
 import zipfile
+import time
 
 Base_URL = "https://api.doc2x.noedgeai.com/api"
 
@@ -58,8 +59,16 @@ def uuid2file(api_key, uuid, output_format, output_path=None):
             f.write(get_res.content)
         if output_format == "zip":
             output_path = un_zip(output_path)
+        print(f"Doc2x remaining page limit: {get_limit(api_key)}")
         return output_path
     else:
+        if get_res.status_code == 429:
+            print("Too many requests, wait for 20s and try again")
+            time.sleep(10)
+            print("10s left")
+            time.sleep(10)
+            temp = uuid2file(api_key, uuid, output_format, output_path)
+            return temp
         raise RuntimeError(
             f"Uuid2file failed, status code: {get_res.status_code}:{get_res.text}"
         )
@@ -94,6 +103,13 @@ def pic2file(
                     line = line[len("data: ") :]
                     text_json.append(json.loads(line))
     else:
+        if get_res.status_code == 429:
+            print("Too many requests, wait for 20s and try again")
+            time.sleep(10)
+            print("10s left")
+            time.sleep(10)
+            temp = pic2file(api_key, image_file, output_path, output_format, img_correction)
+            return temp
         raise RuntimeError(
             f"Pic2file failed, status code: {get_res.status_code}:{get_res.text}"
         )
@@ -105,6 +121,7 @@ def pic2file(
         except:
             pass
     if output_format == "text":
+        print(f"Doc2x remaining page limit: {get_limit(api_key)}")
         return text
     else:
         if output_format not in ["md", "latex", "docx", "md_dollar"]:
@@ -144,6 +161,13 @@ def pdf2file(api_key, pdf_file, output_path=None, output_format="text", ocr=True
                     line = line[len("data: ") :]
                     text_json.append(json.loads(line))
     else:
+        if get_res.status_code == 429:
+            print("Too many requests, wait for 20s and try again")
+            time.sleep(10)
+            print("10s left")
+            time.sleep(10)
+            temp = pdf2file(api_key, pdf_file, output_path, output_format, ocr)
+            return temp
         raise RuntimeError(
             f"Pdf2file failed, status code: {get_res.status_code}:{get_res.text}"
         )
@@ -155,6 +179,7 @@ def pdf2file(api_key, pdf_file, output_path=None, output_format="text", ocr=True
         except:
             pass
     if output_format == "text":
+        print(f"Doc2x remaining page limit: {get_limit(api_key)}")
         return text
     else:
         if output_format not in ["md", "latex", "docx", "md_dollar"]:
@@ -166,6 +191,15 @@ def pdf2file(api_key, pdf_file, output_path=None, output_format="text", ocr=True
         except Exception as e:
             raise RuntimeError(f"Deal with uuid: \n {uuid} \n failed: {e}")
 
+def get_limit(api_key):
+    url = Base_URL + "/platform/limit"
+    get_res = requests.get(url, headers={"Authorization": "Bearer " + api_key})
+    if get_res.status_code == 200:
+        return get_res.json()["data"]["remain"]
+    else:
+        raise RuntimeError(
+            f"Get limit failed, status code: {get_res.status_code}:{get_res.text}"
+        )
 
 class Doc2x:
     def __init__(self, api_key) -> None:
@@ -179,7 +213,19 @@ class Doc2x:
         )
 
     def OCR(self, path, language=["None"], GPU=False):
-        return pic2file(self.key, path, output_format="text", img_correction=True)
+        if os.path.isfile(path) and path.endswith((".jpg", ".png", ".jpeg")):
+            return pic2file(self.key, path, output_format="text", img_correction=True)
+        elif os.path.isdir(path):
+            text = ""
+            for file in os.listdir(path):
+                file_path = os.path.join(path, file)
+                if file.endswith((".jpg", ".png", ".jpeg")):
+                    text += pic2file(self.key, file_path, output_format="text", img_correction=True)
+                    text += "\n"
+            return text
 
     def pdf2file(self, pdf_file, output_path=None, output_format="text", ocr=True):
         return pdf2file(self.key, pdf_file, output_path, output_format, ocr)
+
+    def get_limit(self):
+        return get_limit(self.key)
