39d8c36e866818be6b1ba228900b6576adc7c6db
Menghuan <Menghuan2003@outlook.com>
2024-06-26 23:56:14 +0800

    At least work on 3.10+...

--- src/pdfdeal/Doc2X/Exception.py
+++ src/pdfdeal/Doc2X/Exception.py
@@ -56,11 +56,6 @@ def async_retry(max_retries=3, backoff_factor=2):
 class RateLimiter:
     def __init__(self, rpm):
         self.rpm = rpm
-        try:
-            self.loop = asyncio.get_event_loop()
-        except RuntimeError:
-            self.loop = asyncio.new_event_loop()
-            asyncio.set_event_loop(self.loop)
         self.semaphore = asyncio.Semaphore(rpm)
         self.last_call_times = deque(maxlen=rpm)
         self.lock = asyncio.Lock()
